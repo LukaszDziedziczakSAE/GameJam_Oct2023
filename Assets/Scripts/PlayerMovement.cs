0bd734430bb9a6c1795bb75d60141848f17b082a
Lukasz Dziedziczak <1032314@student.sae.edu.au>
2023-10-30 13:56:05 +1030

    Working on player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] InputReader input;
     [SerializeField] float speed;
+    [SerializeField] Animator animator;
+
+    bool facingRight = true;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +19,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if (input.Movement.magnitude > 0)
         {
-            print("moving");
             Vector3 position = transform.position;
             position.x += input.Movement.x * speed * Time.deltaTime;
-            transform.position = position; 
+            transform.position = position;
+
+            if (input.Movement.x > 0) facingRight = true;
+            else if (input.Movement.x < 0) facingRight = false;
+
+            animator.SetFloat("Speed", 1);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
+
+        if (facingRight)
+        {
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
         }
     }
 }
